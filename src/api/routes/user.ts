778683c7cd4d140d30bd170953fa3c4552801752
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2025-03-10 03:26:09 +0600

    route authentication added

--- src/api/routes/user.ts
+++ src/api/routes/user.ts
@@ -2,21 +2,32 @@ import express from "express";
 import bcrypt from "bcryptjs";
 import { db } from "../../config/db.ts";
 import { users } from "../../schema/index.ts";
+import { authenticate } from "../middleware/authMiddleware.ts";
 
 const router = express.Router();
 
 // Get all users
-router.get("/", async (req, res) => {
+router.get("/", authenticate, async (req, res) => {
+  if (!req.user) {
+    res.status(401).json({ message: "Unauthenticated" });
+    return;
+  }
   try {
     const allUsers = await db.select().from(users);
     res.json(allUsers);
+    return;
   } catch (error) {
     res.status(500).json({ message: error.message });
+    return;
   }
 });
 
 // Create new user
-router.post("/", async (req, res) => {
+router.post("/", authenticate, async (req, res) => {
+  if (!req.user) {
+    res.status(401).json({ message: "Unauthenticated" });
+    return;
+  }
   try {
     const { name, email, password, roleId, username } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -32,8 +43,10 @@ router.post("/", async (req, res) => {
     const newUser = await db.insert(users).values(newValue)
 
     res.status(201).json(newUser);
+    return;
   } catch (error) {
     res.status(500).json({ message: error.message });
+    return;
   }
 });
 

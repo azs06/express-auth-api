efe01a14153436144a2b7cf057054b64572563b5
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2025-03-23 01:57:09 +0600

    added create roles and assign roles to user routes

--- src/api/routes/roles.ts
+++ src/api/routes/roles.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express";
-import { roles, rolePermissions, permissions } from "../../schema/index.ts";
+import { roles, rolePermissions, permissions, userRoles } from "../../schema/index.ts";
 import { authenticate } from "../middleware/authMiddleware.ts";
 import { db } from "../../config/db.ts";
 import { eq } from "drizzle-orm";
@@ -61,6 +61,63 @@ router.get("/", authenticate, async (req: Request, res: Response) => {
     res.status(500).json({ error: "Internal server error" });
   }
 });
+
+router.post("/create", authenticate, async (req: Request, res: Response) => {  
+  const name = req.body.name || ""
+  const description = req.body.description || "";
+  const newValue = {
+    name,
+    description
+  }
+  if (!name || !description) {
+    res.status(400).json({ message: "Name and description are required." });
+    return;
+  }
+  try {
+    const result = await db.insert(roles).values(newValue);
+    res.status(201).json(result);
+    return;
+  } catch (error) {
+    console.error("Error creating role:", error);
+    res.status(500).json({ message: "Internal server error" });
+    return
+  }
+})
+
+router.post("/add-role-user", authenticate, async(req: Request, res: Response) => {
+  const userId = req.body.userId;
+  const roleId = req.body.roleId;
+  const assignedBy = req.body.assignedBy;
+  const newValue: { userId: number; roleId: number; assignedBy?: number } = {
+    userId,
+    roleId,
+  };
+  if(assignedBy){
+    newValue["assigned_by"] = assignedBy
+  }
+  if (!userId || !roleId) {
+    res.status(400).json({ message: "userId and roleId are required." });
+    return;
+  }
+  try {
+    const result = await db.insert(userRoles).values(newValue);
+    res.status(201).json(result);
+    return;
+  } catch (error) {
+    console.error("Error adding role to user:", error);
+    res.status(500).json({ message: "Internal server error" });
+    return
+  }
+
+})
+
+router.post("/update/:id", authenticate, async (req: Request, res: Response) => {  
+  const user = req.body.user;
+  const roles = req.body.permissions;
+
+  // update roles
+
+})
 
 
 export default router;

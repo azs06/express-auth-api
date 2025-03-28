61e910c80f988084da7a5f0dfa293903fa2fc5fa
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2025-03-29 03:27:09 +0600

    added create permissions post request

--- src/api/routes/permissions.ts
+++ src/api/routes/permissions.ts
@@ -15,4 +15,32 @@ router.get("/", authenticate, async (req: Request, res: Response) => {
   }
 });
 
+router.post("/", authenticate, async (req: Request, res: Response) => {
+  try {
+    const { name, description, resourceType, actionType } = req.body;
+    if (!name || !resourceType || !actionType) {
+      res.status(400).json({ error: "Missing required fields" });
+      return;
+    }
+    const newPermission = {
+      name,
+      description,
+      resourceType,
+      actionType,
+    };
+    const id = await db.insert(permissions).values(newPermission).$returningId();
+    if (!id) {
+      res.status(500).json({ error: "Failed to create permission" });
+      return;
+    }
+    // Optionally, you can return the created permission
+    res.status(200).json({message: "Permission created successfully", permission: { id, ...newPermission}});
+    return;
+  } catch (error) {
+    console.error("Error fetching permissions:", error);
+    res.status(500).json({ error: "Internal server error" });
+    return;
+  }
+});
+
 export default router;

ba673c6723dfca4bc4ddeec30af9723e78ff2a77
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2025-04-15 18:24:14 +0600

    added get single user route

--- src/api/routes/user.ts
+++ src/api/routes/user.ts
@@ -105,4 +105,71 @@ router.post("/", authenticate, async (req, res) => {
   }
 });
 
+router.get("/:id", authenticate, async (req, res) => {
+  if (!req.user) {
+    res.status(401).json({ message: "Unauthenticated" });
+    return;
+  }
+  try {
+    const { id } = req.params;
+    const user = await db
+      .select({
+        id: users.id,
+        username: users.username,
+        name: users.name,
+        email: users.email,
+        isActive: users.isActive,
+        createdAt: users.createdAt,
+        updatedAt: users.updatedAt,
+        lastLogin: users.lastLogin,
+        roles: roles.name,
+      })
+      .from(users)
+      .leftJoin(userRoles, eq(userRoles.userId, users.id))
+      .leftJoin(roles, eq(userRoles.roleId, roles.id))
+      .where(eq(users.id, parseInt(id, 10)));
+    if (user.length === 0) {
+      res.status(404).json({ message: "User not found" });
+      return;
+    }
+    const formattedUser = Object.values(
+      user.reduce((acc, user) => {
+        const {
+          id,
+          username,
+          name,
+          email,
+          isActive,
+          createdAt,
+          updatedAt,
+          roles,
+          lastLogin,
+        } = user;
+        if (!acc[id]) {
+          acc[id] = {
+            id,
+            username,
+            name,
+            email,
+            isActive,
+            createdAt,
+            updatedAt,
+            lastLogin,
+            roles: [],
+          };
+        }
+        if (roles) {
+          acc[id].roles.push(roles);
+        }
+        return acc;
+      }, {})
+    );
+    res.json(formattedUser[0]);
+    return;
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+    return;
+  }
+});
+
 export default router;
